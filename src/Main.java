8c62e8d98eb6ef584babbbb91a01ff6eefc05e79
Arch Huang <ArchKitten05@gmail.com>
2022-03-09 09:20:54 -0800

    Started Main Menu - try catch

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Java FTW!");
+        menu("Hey!", new String[] {"1", "2", "3"});
+    }
+
+    public static void menu(String banner, String[] options_array) {
+        Scanner scanObj = new Scanner(System.in);
+
+        System.out.println(banner);
+        for (String s : options_array) {
+            System.out.println(s);
+        }
+
+        String userInput = scanObj.nextLine();
+
+        try {
+            int selection = Integer.parseInt(userInput);
+        }
+        catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
     }
 }
