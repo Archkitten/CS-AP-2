46d0aa43b7bebc3e3ac5fee174050e59d0b29f7d
Arch Huang <ArchKitten05@gmail.com>
2022-03-11 13:55:10 -0800

    Python number_swap and matrix_to_text complete

--- src_py/matrix_to_text.py
+++ src_py/matrix_to_text.py
@@ -1,9 +1,15 @@
 class MatrixToText:
     def __init__(self):
         self.name = "matrix_to_text"
+        self.numpad = [[1, 2, 3],
+                       [4, 5, 6],
+                       [7, 8, 9]]
 
     def get_name(self):
         return self.name
 
     def process(self):
-        pass
+        for i in self.numpad:
+            for j in i:
+                print(j, "", end="")
+            print()
--- src_py/number_swap.py
+++ src_py/number_swap.py
@@ -1,9 +1,18 @@
 class NumberSwap:
     def __init__(self):
         self.name = "number_swap"
+        self.a = 0
+        self.b = 0
 
     def get_name(self):
         return self.name
 
     def process(self):
-        pass
+        self.a = input("Input first integer: ")
+        self.b = input("Input second integer: ")
+        # Wait what? Why? This shouldn't work...
+        if self.a < self.b:
+            temp = self.a
+            self.a = self.b
+            self.b = temp
+        print(self.a, self.b)
