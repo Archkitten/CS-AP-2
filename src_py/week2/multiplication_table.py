d0d19cc86c24e413787a443e9730bd4e1e84754a
Arch Huang <Archkitten@users.noreply.github.com>
2022-03-25 14:19:47 -0700

    Python - MultiplicationTable better system, no longer scalable with higher number of digits

--- src_py/week2/multiplication_table.py
+++ src_py/week2/multiplication_table.py
@@ -7,19 +7,18 @@ class MultiplicationTable(Option):
         self.name = "Multiplication Table"
 
     def __call__(self):
-        t = Tables10To99()
-        t(87)
+        a = Tables10To99(87)
+        a()
         print()
-        t(38)
+        b = Tables10To99(38)
+        b()
         print()
-        t(92)
+        c = Tables10To99(92)
+        c()
 
 
 class Tables10To99:
-    def __init__(self):
-        self.table_size = 0
-
-    def __call__(self, number):
+    def __init__(self, number):
         # Convert int into string.
         number_string = str(number)
         # Convert string into array.
@@ -29,25 +28,42 @@ class Tables10To99:
         for s in string_array:
             number_array.append(int(s))
         # Sort array in order to start from the highest digit.
-        number_array.sort(reverse=True)
+        self.flipped = False
+        if number_array[0] < number_array[1]:
+            number_array.reverse()
+            self.flipped = True
 
-        # Iterate through array, get each digit's factors.
-        for digit in number_array:
-            # Store factors of digit.
-            factor_array = []
-            # Factor counter.
-            if self.table_size == 0:
-                d = digit
-                while digit < 100:
-                    digit += d
-                    factor_array.append(digit)
-                    self.table_size += 1
-            else:
-                i = 0
-                d = digit
-                while i < self.table_size:
-                    digit += d
-                    factor_array.append(digit)
-            print(factor_array)
+        # Constructor
+        self.higher_digit = number_array[0]
+        self.lower_digit = number_array[1]
 
+        self.higher_digit_array = []
+        self.lower_digit_array = []
 
+        self.table_size = 0
+
+    def __call__(self):
+        self.create_higher_digit_factors(self.higher_digit)
+        self.create_lower_digit_factors(self.lower_digit)
+
+        print(self.higher_digit_array)
+        print(self.lower_digit_array)
+
+    def create_higher_digit_factors(self, digit):
+        # Digit constant
+        d = digit
+        # Get the factors, while also recording the max iterations for the other digit.
+        while digit < 100:
+            digit += d
+            self.higher_digit_array.append(digit)
+            self.table_size += 1
+
+    def create_lower_digit_factors(self, digit):
+        # Digit constant and iterator
+        d = digit
+        i = 0
+        # Get the factors.
+        while i < self.table_size:
+            digit += d
+            self.lower_digit_array.append(digit)
+            i += 1
