33d42009308fc0fb63fd022b1171cf91fe966942
Arch Huang <Archkitten@users.noreply.github.com>
2022-03-25 14:03:01 -0700

    Python - MultiplicationTable checkpoint, need to rework

--- src_py/week2/multiplication_table.py
+++ src_py/week2/multiplication_table.py
@@ -9,13 +9,45 @@ class MultiplicationTable(Option):
     def __call__(self):
         t = Tables10To99()
         t(87)
+        print()
         t(38)
+        print()
         t(92)
 
 
 class Tables10To99:
     def __init__(self):
-        pass
+        self.table_size = 0
 
     def __call__(self, number):
-        pass
+        # Convert int into string.
+        number_string = str(number)
+        # Convert string into array.
+        string_array = list(number_string)
+        # Turn array of Strings back into array of ints.
+        number_array = []
+        for s in string_array:
+            number_array.append(int(s))
+        # Sort array in order to start from the highest digit.
+        number_array.sort(reverse=True)
+
+        # Iterate through array, get each digit's factors.
+        for digit in number_array:
+            # Store factors of digit.
+            factor_array = []
+            # Factor counter.
+            if self.table_size == 0:
+                d = digit
+                while digit < 100:
+                    digit += d
+                    factor_array.append(digit)
+                    self.table_size += 1
+            else:
+                i = 0
+                d = digit
+                while i < self.table_size:
+                    digit += d
+                    factor_array.append(digit)
+            print(factor_array)
+
+
