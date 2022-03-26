750ddbb4692f95008e5df378f9b74a10e336f306
Arch Huang <archkitten05@gmail.com>
2022-03-25 21:19:41 -0700

    DBF - Intro and fade

--- p2/deep_blue_fish/intro.py
+++ p2/deep_blue_fish/intro.py
@@ -1,16 +1,70 @@
 from p2.deep_blue_fish.util.ui_menu import UIMenu
-from p2.deep_blue_fish.util.ui_elements import *
+from p2.deep_blue_fish.util.ui_elements import Text, Picture
 from p2.deep_blue_fish.util.config import *
 import pygame
+import random
 
 
 class Intro(UIMenu):
     def __init__(self):
         super().__init__()
-        self.title = Text("Intro", WIN_WIDTH / 2, 200, 'Blue', pygame.font.SysFont('pristina', int(100 * WIN_SCALE)))
+        # Quotes
+        self.quotes = ["If I could, I would.",
+                       "Conquer All!",
+                       "As if.",
+                       "the876",
+                       "And countless more on memories...",
+                       "I think this guy is possessed",
+                       "Ma Clique",
+                       "Wait, who is michael?",
+                       "Cover your bald head",
+                       "ciao!",
+                       "we shall fight in Lakliin",
+                       "My signature move: The Backstabbing Victory",
+                       "T H E  W A L L",
+                       "We'll burn that bridge when we get to it",
+                       "L'Mao",
+                       "Taking too long.",
+                       "02/11/2018",
+                       "Au",
+                       "feesh",
+                       "Under Pressure",
+                       "Nay I say!",
+                       "Ohn",
+                       "SCP-8704 and Protocol 520-Gan"]
+        q = random.randint(-3, len(self.quotes) - 1)
+        if q == -1:
+            self.quote = Text('"Instead of fading everything out, just make Black.png fade in" - Q', 800, 450, 'Blue',
+                              pygame.font.SysFont('cambria', int(50 * WIN_SCALE)))
+        elif q == -2:
+            self.quote = Text("BOTTOM TEXT", 800, 800, 'Blue',
+                              pygame.font.SysFont('impact', int(100 * WIN_SCALE)))
+        elif q == -3:
+            self.quote = Text("To each it is left unknown, that to each it is granted, this gift brings a curse; that every memory of something as a part of anything can become everything in an instant, as existence in this place and time brings a power unlike any other to create that which can create, and to manifest something in the mind of anyone, that beckons their soul to the heavens of Earth.",
+                              800, 450, 'White', pygame.font.SysFont('pristina', int(14 * WIN_SCALE)))
+        else:
+            self.quote = Text(self.quotes[q], 800, 450, 'Blue', pygame.font.SysFont('pristina', int(80 * WIN_SCALE)))
+        # Black.png
+        self.black = Picture('img/Black.png', 800, 450, 1)
+        self.timer = 255
+        self.timer_phase = "Fading out, words appearing"
 
     def while_loop(self):
         self.SCREEN.fill('Black')
-        self.title(self.SCREEN)
+        self.quote(self.SCREEN)
+        self.black(self.SCREEN, self.timer)
+        # Skip with left click.
         if self.left_click:
             self.running = False
+        # Fading or unfading black. 4 and 3 are the fading and unfading speeds, respectively.
+        if self.timer_phase == "Fading out, words appearing":
+            self.timer -= 4
+        else:
+            self.timer += 3
+        # Switch from fading to unfading. -43 is the delay.
+        if self.timer <= -43:
+            self.timer_phase = "Fading in, words disappearing"
+        # End intro after fading complete. 43 is the delay.
+        if self.timer > 255 + 43:
+            self.running = False
+
--- p2/deep_blue_fish/util/ui_elements.py
+++ p2/deep_blue_fish/util/ui_elements.py
@@ -1,4 +1,5 @@
 from p2.deep_blue_fish.util.config import *
+import pygame
 
 
 class Text:
@@ -9,9 +10,13 @@ class Text:
     def __call__(self, screen):
         screen.blit(self.text, self.text_rect)
 
-class Picture:
-    def __init__(self):
-        pass
 
-    def __call__(self):
-        pass
+class Picture:
+    def __init__(self, image, x, y, scale):
+        self.image = pygame.image.load(image).convert_alpha()
+        self.image = pygame.transform.scale(self.image, (self.image.get_width() * WIN_SCALE * scale, self.image.get_height() * WIN_SCALE * scale))
+        self.image_rect = self.image.get_rect(center=(x * WIN_SCALE, y * WIN_SCALE))
+
+    def __call__(self, screen, opacity):
+        self.image.set_alpha(opacity)
+        screen.blit(self.image, self.image_rect)
