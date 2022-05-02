7e8dd802a531e431cd89a631dc6a616180ffc4f1
Arch Huang <archkitten05@gmail.com>
2022-05-01 18:05:23 -0700

    Java - 2015 FRQ4 started part 2

--- src/csa/week5/NumberGroup.java
+++ src/csa/week5/NumberGroup.java
@@ -1,4 +1,7 @@
 package csa.week5;
 
 public interface NumberGroup {
+
+    public boolean isInGroup();
+
 }
--- src/csa/week5/Range.java
+++ src/csa/week5/Range.java
@@ -1,5 +1,28 @@
 package csa.week5;
 
-public class Range {
+public class Range implements NumberGroup {
 
+    private int[] range;
+
+    public Range(int a, int b) {
+        range = new int[b - a];
+
+        for (int i = 0; i < range.length; i++) {
+            range[i] = a + i;
+        }
+    }
+
+    // Delete this later, used for checking
+    public int[] getRange() {
+        return range;
+    }
+
+    @Override
+    public boolean isInGroup() {
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
